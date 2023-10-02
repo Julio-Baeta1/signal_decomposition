961e81c75c6d598377db2e0b64752b5845088865
Julio-Baeta1 <julbay1643@gmail.com>
2023-10-02 16:37:05 +0200

    Fix: Made W a property with setting functions

--- include/ica.h
+++ include/ica.h
@@ -14,6 +14,7 @@ using Mat = Eigen::MatrixXd;
 class Ica{
     private:
         std::shared_ptr<Mat> X;
+        std::unique_ptr<Mat> W;
     public:
 
         Ica(std::shared_ptr<Mat> ini_X = nullptr);
@@ -22,8 +23,12 @@ class Ica{
         void setSourceFromFile(std::string filename);
         std::shared_ptr<Mat> getResultPtr() {return X;}
 
+        void randW(int rows, int cols, int seed);
+        void setW(int rows, int cols, int seed, bool is_rand);
+
         void sphering();
         void decompose(int n_sigs, bool rand_W, int seed);
+        void fastIca(int n_sigs);
 };
 
 #endif
--- src/ica.cpp
+++ src/ica.cpp
@@ -3,16 +3,20 @@
 Ica::Ica(std::shared_ptr<Mat> ini_X)
 {
     X = std::move(ini_X);
+    W = nullptr;//std::make_unique<Mat>(Mat::Identity(X->cols(),X->cols())) ;
 }
 
 void Ica::setSource(std::shared_ptr<Mat> new_X)
 {
+    std::cout << "Inside set Source" << std::endl;
     X = std::move(new_X);
 }
+
 
 void Ica::setSourceFromFile(std::string filename)
 //Currently hardcoded to scikit-learn example
 {
+    std::cout << "Inside Source from file" << std::endl;
     Mat SS = Mat::Zero(2000,3);
     Mat A{{1, 1, 1}, {0.5, 2, 1.0}, {1.5, 1.0, 2.0}};
 
@@ -35,6 +39,22 @@ void Ica::setSourceFromFile(std::string filename)
     //X = std::make_shared<Mat>(SS*A.transpose());
 }
 
+void Ica::randW(int rows, int cols, int seed)
+{
+    std::mt19937 r_gen (seed);
+    std::normal_distribution<double> r_dis(0.0, 1.0);
+    auto norm = [&] () {return r_dis(r_gen);};
+
+    W = std::make_unique<Mat>(Mat::NullaryExpr(rows,cols, norm )) ;
+}
+
+void Ica::setW(int rows, int cols, int seed, bool is_rand)
+{
+    if(is_rand)
+        randW(rows, cols, seed);
+    else
+        W = std::make_unique<Mat>(Mat::Identity(rows,cols)) ;
+}
 
 void Ica::sphering()
 {
@@ -59,17 +79,7 @@ void Ica::decompose(int n_sigs, bool rand_W, int seed)
 /* Gradient Descent of Entropy
 */
 {   
-    Mat W = Mat::Identity(n_sigs,n_sigs);
-
-    if(rand_W)
-    {
-        std::cout << "Using Random W with seed:" << seed << std::endl;
-        std::mt19937 r_gen (seed);
-        std::normal_distribution<double> r_dis(0.0, 1.0);
-        auto norm = [&] () {return r_dis(r_gen);};
-
-        W = Mat::NullaryExpr(n_sigs,n_sigs, norm );
-    }
+    setW(n_sigs,n_sigs,seed,rand_W);
 
     double norm_const = 2/(double)X->cols();
     Mat new_W = Mat::Zero(n_sigs,n_sigs);
@@ -80,11 +90,31 @@ void Ica::decompose(int n_sigs, bool rand_W, int seed)
 
     for(int i=0; i<max_iter; i++)
     {
-        u = W * *X;
+        //u = W * *X;
+        u = *W * *X;
         U = u.array().tanh();
         new_W = norm_const* *X * U.transpose();
-        new_W = W.transpose().inverse() - new_W; 
-        W = W + step*new_W;
+        //new_W = W.transpose().inverse() - new_W; 
+        //W = W + step*new_W;
+        new_W = W->transpose().inverse() - new_W; 
+        *W = *W + step*new_W;
     }
-    *X = W * *X;     
+    *X = *W * *X;     
 }
+
+/*void Ica::fastIca(int n_sigs)
+{
+    sphering();
+    Mat XX = X->transpose();
+
+    int M = XX.rows(), N= XX.cols();
+    int max_iter = 1000;
+
+    Mat W
+
+    for(int i=0; i<max_iter; i++)
+    {
+
+    }
+
+}*/
